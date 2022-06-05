8c8539386fc97f7928d07d7e83d705b81c79f9d9
AhmedMaad <atef.ahmed1341@gmail.com>
2022-06-05 23:31:49 +0200

    deleteing every thing when reset is clicked

--- app/src/main/java/com/maad/eta/hr/HRHomeActivity.kt
+++ app/src/main/java/com/maad/eta/hr/HRHomeActivity.kt
@@ -5,19 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.maad.eta.LoginActivity
 import com.maad.eta.R
 import com.maad.eta.databinding.ActivityHrhomeBinding
+import com.maad.eta.employee.SickRequest
 
 class HRHomeActivity : AppCompatActivity() {
+
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityHrhomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = "HR Home"
+        db = Firebase.firestore
 
         binding.annualCv.setOnClickListener {
             startActivity(Intent(this, ViewAnnualVacationActivity::class.java))
@@ -55,7 +62,25 @@ class HRHomeActivity : AppCompatActivity() {
     * This function should be used at the beginning of each year
     * */
     private fun restoreEverything() {
-
+        db.collection("SickRequests").get().addOnSuccessListener { x ->
+            val allSick = x.documents
+            for (sick in allSick)
+                db.collection("SickRequests").document(sick.getString("requestId")!!)
+                    .delete()
+            db.collection("annualVacations").get().addOnSuccessListener { y ->
+                val allVacations = y.documents
+                for (sick in allVacations)
+                    db.collection("annualVacations").document(sick.getString("requestId")!!)
+                        .delete()
+                db.collection("trackers").get().addOnSuccessListener { z ->
+                    val trackers = z.documents
+                    for (sick in trackers)
+                        db.collection("trackers").document(sick.getString("requestId")!!)
+                            .delete()
+                    Toast.makeText(this, "Everything is now deleted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
 }
